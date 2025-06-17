8699da23e5235f6f084d2158bb1d2ebb88e0e374
Tinho <michealugwu16@gmail.com>
2025-06-17 09:00:53 +0100

    ''

--- ASSETS/JS/Main.js
+++ ASSETS/JS/Main.js
@@ -1,13 +1,6 @@
 
 $(document).ready(function() {
-  $('.marquee').marquee({
-    duration: 9000,         // how fast it scrolls (ms)
-    gap: 50,                 // space between loops
-    delayBeforeStart: 0,
-    direction: 'left',       // can also do 'up' for vertical
-    duplicated: true,        // loops back seamlessly
-    pauseOnHover: false      // slows down or pauses on hover
-  });
+ 
 
 
   let typed = new Typed('#name-intro', {

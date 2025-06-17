77b6895cdfd8debcc2c7e2112f6b23e7d8698daf
Tinho <michealugwu16@gmail.com>
2025-06-17 09:29:38 +0100

    ''

--- ASSETS/JS/Main.js
+++ ASSETS/JS/Main.js
@@ -1,6 +1,27 @@
 
 $(document).ready(function() {
  
+  const marqueeTrack = $(".marquee-track");
+  const images = marqueeTrack.find("img")
+  let LoadedCount = 0;
+
+  images.each(function() {
+    let img = $(this);
+
+    if (img[0].complete) {
+      LoadedCount++;
+      if(LoadedCount === images.length) {
+        marqueeTrack.addClass("animate-marquee")
+      }
+    }else{
+      $(img).on("load", function () {
+        LoadedCount++;
+        if(LoadedCount === images.length) {
+          marqueeTrack.addClass("animate-marquee")
+        }
+      })
+    }
+  });
 
 
   let typed = new Typed('#name-intro', {
